86b49698acab906b5f2162915e8e1f69794e3164
Thomas Gotwig <tgotwig@gmail.com>
2019-12-11 18:17:16 +0100

    Move data from Accordion-View to model

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,4 +6,42 @@ import { Component } from '@angular/core'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  data = [
+    {
+      name: 'Github',
+      class: 'fa-github',
+      href: 'https://github.com/TGotwig',
+      text: 'Check out my Github for personal projects and more 🐱'
+    },
+    {
+      name: 'Twitter',
+      class: 'fa-twitter',
+      href: 'https://twitter.com/tgotwig',
+      text: 'Follow me on Twitter for programming related posts in future 🦋'
+    },
+    {
+      name: 'Linkedin',
+      class: 'fa-linkedin-in',
+      href: 'https://www.linkedin.com/in/thomas-gotwig-244a35177',
+      text: 'Connect with me on a professional level on Linkedin 🤗'
+    },
+    {
+      name: 'Kaggle',
+      class: 'fa-kaggle',
+      href: 'https://www.kaggle.com/thomasgotwig',
+      text: 'Check out my Kaggle to see on which challenges I currently compete 🤖 an older account is also available under the URL-ending "tgotwig" 🤓'
+    },
+    {
+      name: 'Stackoverflow',
+      class: 'fa-stack-overflow',
+      href: 'https://stackoverflow.com/users/6244047/thomas-gotwig',
+      text: 'Check out my contributions on Stackoverflow 📚'
+    },
+    {
+      name: 'Medium',
+      class: 'fa-medium-m',
+      href: 'https://medium.com/@tgotwig',
+      text: 'Check out my writings on Medium ✍️'
+    },
+  ]
 }
