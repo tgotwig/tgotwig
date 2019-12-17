cae54975bcc9011e9983620827680890c831d89c
Thomas Gotwig <tgotwig@gmail.com>
2019-12-17 20:20:05 +0100

    Rename assets

--- src/app/components/certifications/certifications.component.ts
+++ src/app/components/certifications/certifications.component.ts
@@ -12,20 +12,20 @@ export class CertificationsComponent {
       name: 'Education',
       cert: [
         {
-          preview: 'assets/img/bachelor.png',
-          download: 'assets/pdf/bachelor.pdf'
+          preview: 'assets/img/thm-bachelor.png',
+          download: 'assets/pdf/thm-bachelor.pdf'
         },
         {
-          preview: 'assets/img/synmikro.jpg',
-          download: 'assets/pdf/synmikro.pdf'
+          preview: 'assets/img/conference-synmikro.jpg',
+          download: 'assets/pdf/conference-synmikro.pdf'
         },
         {
-          preview: 'assets/img/gcb2017.jpg',
-          download: 'assets/pdf/gcb2017.pdf'
+          preview: 'assets/img/conference-gcb2017.jpg',
+          download: 'assets/pdf/conference-gcb2017.pdf'
         },
         {
-          preview: 'assets/img/gcb2015.jpg',
-          download: 'assets/pdf/gcb2015.pdf'
+          preview: 'assets/img/conference-gcb2015.jpg',
+          download: 'assets/pdf/conference-gcb2015.pdf'
         }
       ]
     },
@@ -33,28 +33,28 @@ export class CertificationsComponent {
       name: 'Jobs',
       cert: [
         {
-          preview: 'assets/img/arago.jpg',
-          download: 'assets/pdf/arago.pdf'
+          preview: 'assets/img/job-arago.jpg',
+          download: 'assets/pdf/job-arago.pdf'
         },
         {
-          preview: 'assets/img/jlu.jpg',
-          download: 'assets/pdf/jlu.pdf'
+          preview: 'assets/img/job-jlu.jpg',
+          download: 'assets/pdf/job-jlu.pdf'
         },
         {
-          preview: 'assets/img/tci.jpg',
-          download: 'assets/pdf/tci.pdf'
+          preview: 'assets/img/internship-tci.jpg',
+          download: 'assets/pdf/internship-tci.pdf'
         },
         {
-          preview: 'assets/img/infobase.jpg',
-          download: 'assets/pdf/infobase.pdf'
+          preview: 'assets/img/internship-infobase.jpg',
+          download: 'assets/pdf/internship-infobase.pdf'
         },
         {
-          preview: 'assets/img/hagel.jpg',
-          download: 'assets/pdf/hagel.pdf'
+          preview: 'assets/img/internship-hagel.jpg',
+          download: 'assets/pdf/internship-hagel.pdf'
         },
         {
-          preview: 'assets/img/thm.jpg',
-          download: 'assets/pdf/thm.pdf'
+          preview: 'assets/img/internship-thm.jpg',
+          download: 'assets/pdf/internship-thm.pdf'
         }
       ]
     }
