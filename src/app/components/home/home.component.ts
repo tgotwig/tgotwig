2fd148c8089dd93392d81cc9db30e17b3090632e
tgotwig <tgotwig@gmail.com>
2020-08-14 23:26:46 +0200

    Add super-linter 🚀

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,50 +1,50 @@
-import { Component, OnInit } from '@angular/core'
-import { StoreService } from '../../store.service'
+import { Component, OnInit } from "@angular/core"
+import { StoreService } from "../../store.service"
 declare var gsap: any
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
   data = [
     {
-      name: 'Github',
-      class: 'fa-github',
-      href: 'https://github.com/TGotwig',
-      text: 'Check out my Github for personal projects and more 🐱'
+      name: "Github",
+      class: "fa-github",
+      href: "https://github.com/TGotwig",
+      text: "Check out my Github for personal projects and more 🐱",
     },
     {
-      name: 'Linkedin',
-      class: 'fa-linkedin-in',
-      href: 'https://www.linkedin.com/in/thomas-gotwig-244a35177',
-      text: 'Connect with me on a professional level on Linkedin 🤗'
+      name: "Linkedin",
+      class: "fa-linkedin-in",
+      href: "https://www.linkedin.com/in/thomas-gotwig-244a35177",
+      text: "Connect with me on a professional level on Linkedin 🤗",
     },
     {
-      name: 'Stackoverflow',
-      class: 'fa-stack-overflow',
-      href: 'https://stackoverflow.com/users/6244047/thomas-gotwig',
-      text: 'Check out my contributions on Stackoverflow 📚'
+      name: "Stackoverflow",
+      class: "fa-stack-overflow",
+      href: "https://stackoverflow.com/users/6244047/thomas-gotwig",
+      text: "Check out my contributions on Stackoverflow 📚",
     },
     {
-      name: 'Medium',
-      class: 'fa-medium-m',
-      href: 'https://medium.com/@tgotwig',
-      text: 'Check out my writings on Medium ✍️'
+      name: "Medium",
+      class: "fa-medium-m",
+      href: "https://medium.com/@tgotwig",
+      text: "Check out my writings on Medium ✍️",
     },
   ]
 
   ngOnInit() {
     if (this.store.HOME_ANIMATION) {
-      gsap.from('#left', { opacity: 0, duration: 1, x: -50, y: -50 })
-      gsap.from('#right', { opacity: 0, duration: 1, x: 50, y: -50 })
-      gsap.from('#block1', { opacity: 0, duration: 1.5, delay: 0.4, })
-      gsap.from('#block2', { opacity: 0, duration: 1.5, delay: 0.6, })
-      gsap.from('#block3', { opacity: 0, duration: 1.5, delay: 0.8, })
-      gsap.from('.material-icons', { opacity: 0, delay: 1, duration: 2 })
+      gsap.from("#left", { opacity: 0, duration: 1, x: -50, y: -50 })
+      gsap.from("#right", { opacity: 0, duration: 1, x: 50, y: -50 })
+      gsap.from("#block1", { opacity: 0, duration: 1.5, delay: 0.4 })
+      gsap.from("#block2", { opacity: 0, duration: 1.5, delay: 0.6 })
+      gsap.from("#block3", { opacity: 0, duration: 1.5, delay: 0.8 })
+      gsap.from(".material-icons", { opacity: 0, delay: 1, duration: 2 })
     } else {
-      gsap.from('#left,#right,#block1,#block2,#block3', { opacity: 0, })
+      gsap.from("#left,#right,#block1,#block2,#block3", { opacity: 0 })
     }
     this.store.HOME_ANIMATION = false
   }
